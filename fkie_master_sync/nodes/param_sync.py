fccdf5a75c01941d67f412be59dc4b7bac0cc36a
Charlie-Burge <60987211+Charlie-Burge@users.noreply.github.com>
2021-01-26 17:43:34 +0000

    sync params across masters

--- fkie_master_sync/nodes/param_sync.py
+++ fkie_master_sync/nodes/param_sync.py
@@ -8,51 +8,48 @@ from fkie_master_discovery.common import masteruri_from_master
 from fkie_multimaster_msgs.msg import MasterState
 
 def master_changed(msg, cb_args):
-    param_cache, local_master, __add_ns = cb_args
-    local_name = ''
-    if local_master:
-        local_name = local_master[0]
-    if msg.master.uri != masteruri_from_master() and local_name in param_cache:
-        master_to = rospy.MasterProxy(masteruri_from_master())
-        master_from = rospy.MasterProxy(msg.master.uri)
-        rospy.logdebug("Getting params from {}...".format(msg.master.uri))
-        params_from = master_from['/']
-        rospy.logdebug("Got {} params.".format(len(msg.master.uri)))
-        if local_name in params_from:
-            del params_from[local_name]
-        if '/'+local_name in params_from:
-            del params_from['/'+local_name]
-        rospy.logdebug("Syncing params from {} to {}...".format(msg.master.name, local_name))
-        if __add_ns:
-            _ns = msg.master.name
-        else:
-            _ns = ''
-        if param_cache.get(_ns, None) != params_from:
-            param_cache[_ns] = params_from
-            master_to['/'+_ns] = params_from
-            rospy.logdebug("Done syncing params from {} to {}.".format(msg.master.name, local_name))
-        else:
-            rospy.logdebug("Params have not changed from {} to {}.".format(msg.master.name, local_name))
-    else:
-        local_name = msg.master.name
-        local_master.append(local_name)
-        master_from = rospy.MasterProxy(msg.master.uri)
-        rospy.logdebug("Getting params from local {}...".format(msg.master.uri))
-        param_cache[local_name] = master_from['/']
-        rospy.logdebug("Got {} params.".format(len(msg.master.uri)))
+    master_proxies, param_cache, __add_ns = cb_args
+    master_proxies[msg.master.name] = rospy.MasterProxy(msg.master.uri)
+
+    for name_from, master_from in master_proxies.items():
+        for name_to, master_to in master_proxies.items():
+            if name_to != name_from:
+                rospy.logdebug("Getting params from...".format(name_from))
+                params_from = master_from['/']
+                rospy.logdebug("Got {} params.".format(len(params_from)))
+                if name_to in params_from:
+                    del params_from[name_to]
+                if '/'+name_to in params_from:
+                    del params_from['/'+name_to]
+                rospy.logdebug("Syncing params from {} to {}...".format(name_from, name_to))
+                if __add_ns:
+                    _ns = name_from
+                else:
+                    _ns = ''
+                if param_cache.get(_ns, None) != params_from:
+                    param_cache[_ns] = params_from
+                    master_to['/'+_ns] = params_from
+                    rospy.logdebug("Done syncing params from {} to {}.".format(name_from, name_to))
+                else:
+                    rospy.logdebug("Params have not changed from {} to {}.".format(name_from, name_to))
 
 
 def main():
-    rospy.init_node('param_sync', log_level=rospy.DEBUG)
+    rospy.init_node('param_sync')
 
+    master_proxies = dict()
     param_cache = dict()
-    local_master = list()
-    masteruri_from_master()
 
-    __add_ns = rospy.get_param('~add_ns', True)
-    sub = rospy.Subscriber('master_discovery/changes', MasterState, master_changed, callback_args=(param_cache, local_master, __add_ns))
+    __add_ns = rospy.get_param('~add_ns', False)
+    sub = rospy.Subscriber(
+        'master_discovery/changes', 
+        MasterState, 
+        master_changed, 
+        callback_args=(master_proxies, param_cache, __add_ns)
+    )
 
     rospy.spin()
 
 if __name__ == '__main__':
     main()
+
